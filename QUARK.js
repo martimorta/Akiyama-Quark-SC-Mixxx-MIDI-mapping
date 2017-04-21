f932c4ab502d1382da9e9ebb73f391c69e008765
Kubi <email@jakubkrizka.cz>
2017-04-21 22:03:48 +0200

    update MIDI, rename for auto-script

--- QUARK.js
+++ QUARK.js
@@ -2,6 +2,7 @@
 
 var QUARK = {};
 
+
 // The wheel that actually controls the scratching quickly with shift
 QUARK.shiftWheelTurn1 = function (channel, control, value) {
     // B: For a control that centers on 0x40 (64):
@@ -12,7 +13,7 @@ QUARK.shiftWheelTurn1 = function (channel, control, value) {
         engine.scratchTick(1, newValue);
         engine.scratchDisable(1);
     } else {
-        engine.setValue('[Channel'+1+']', 'jog', newValue*6);
+        engine.setValue('[Channel'+1+']', 'jog', newValue*8);
         engine.scratchDisable(1);
     }
 };
@@ -33,12 +34,12 @@ QUARK.wheelTurn1 = function (channel, control, value) {
 // .and for dack 2 etc...
 QUARK.shiftWheelTurn2 = function (channel, control, value) {
     var newValue = value - 64;
- 
+
     if (engine.isScratching(2)) {
         engine.scratchTick(2, newValue);
         engine.scratchDisable(2);
     } else {
-        engine.setValue('[Channel'+2+']', 'jog', newValue*6);
+        engine.setValue('[Channel'+2+']', 'jog', newValue*8);
         engine.scratchDisable(2);
     }
 };
@@ -62,7 +63,7 @@ QUARK.shiftWheelTurn3 = function (channel, control, value) {
         engine.scratchTick(3, newValue);
         engine.scratchDisable(3);
     } else {
-        engine.setValue('[Channel'+3+']', 'jog', newValue*6);
+        engine.setValue('[Channel'+3+']', 'jog', newValue*9);
         engine.scratchDisable(3);
     }
 };
@@ -86,7 +87,7 @@ QUARK.shiftWheelTurn4 = function (channel, control, value) {
         engine.scratchTick(4, newValue);
         engine.scratchDisable(4);
     } else {
-        engine.setValue('[Channel'+4+']', 'jog', newValue*6);
+        engine.setValue('[Channel'+4+']', 'jog', newValue*9);
         engine.scratchDisable(4);
     }
 };
